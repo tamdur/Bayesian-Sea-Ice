619a383b70998865bf13fafc64ca8c1f0686184b
Ted Amdur <teda@dhcp-140-247-105-132.fas.harvard.edu>
2022-11-14 13:35:25 -0500

    Add bash script for cluster execution

---
+++ coverage_leave_one_out.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J coverage_leave_one_out
+#SBATCH -o coverage_leave_one_out.out
+#SBATCH -e coverage_leave_one_out.err
+#SBATCH -N 1
+#SBATCH -c 32
+#SBATCH -t 0-15:30
+#SBATCH -p huce_cascade
+#SBATCH --mem=100G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=amdur@g.harvard.edu
+
+module load matlab/R2018b-fasrc01
+srun -c $SLURM_CPUS_PER_TASK matlab -nosplash -nodesktop -r "posterior_estimate_cmip_cluster('model_predictions_22_11_14.mat')"
